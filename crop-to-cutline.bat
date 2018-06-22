e914b55c02ffa43d8615f1084e60e830b2aa2361
Erin Austin <erinjayaustin@gmail.com>
2018-06-22 15:51:12 +0100

    Delete crop-to-cutline.bat

--- crop-to-cutline.bat
+++
@@ -1,3 +0,0 @@
-echo off
-set "plan=BSH1120-1-2018-02.tif"
-gdalwarp -cutline "cutlines.shp" -csql "select * from cutlines where location='%plan%'" -crop_to_cutline -of GTiff -srcnodata -9999 -dstnodata -9999 BSH1120-0-2018-02.tif %plan%
