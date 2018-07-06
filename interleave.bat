b7975e344744cdca2f79bf46771ca3ab5779a998
Erin Austin <erinjayaustin@gmail.com>
2018-07-06 15:43:20 +0100

    Update and rename interleave-batch.bat to interleave.bat

--- interleave.bat
+++ interleave.bat
@@ -1,14 +1,22 @@
 @echo off
 setlocal enabledelayedexpansion
-set oldpath=C:\
-set newpath=C:\
+
+set par1=%1
+set par2=%2
+
+set oldpath=%par1%
+set newpath=%par2%
+
 set numFiles=0
 for %%x in (%oldpath%*.tif) do (
   set file[!numFiles!]=%%~nfx
   set /a numFiles+=1
 )
+
 echo Number of files to interleave: %numFiles%
+ 
 set count=1
+
 for /f %%i in ('dir /B %oldpath%*.tif') do (
 	echo Starting !count! of %numFiles%	
 	gdal_translate -q -co "INTERLEAVE=PIXEL" -co "COMPRESS=LZW" "%oldpath%%%i" "%newpath%%%i"
