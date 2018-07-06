ace4ef3b1d5403467fb1ef7a9823015e4237b7e1
Erin Austin <erinjayaustin@gmail.com>
2018-07-06 11:20:25 +0100

    Update interleave-batch.bat

--- interleave-batch.bat
+++ interleave-batch.bat
@@ -1,24 +1,18 @@
 @echo off
-
 REM Variables expanded at execution time rather than parse time
 setlocal enabledelayedexpansion
-
 REM Source and destination folders
 set oldpath=C:\
 set newpath=C:\
-
 REM count files
 set numFiles=0
 for %%x in (%oldpath%*.tif) do (
   set file[!numFiles!]=%%~nfx
   set /a numFiles+=1
 )
-
 echo Number of files to interleave: %numFiles%
- 
 set count=1
-
-REM gdal_translate switches : -q suppress output : LZW compression : INTERLEAVE pixel
+REM gdal_translate switche: -q to suppress output, LZW compression, INTERLEAVE by pixel
 for /f %%i in ('dir /B %oldpath%*.tif') do (
 	echo Starting !count! of %numFiles%	
 	gdal_translate -q -co "INTERLEAVE=PIXEL" -co "COMPRESS=LZW" "%oldpath%%%i" "%newpath%%%i"
